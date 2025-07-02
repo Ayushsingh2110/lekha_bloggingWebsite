e7ddb50122c60ee33a44c43e795d2893ef898c49
Ayush Singh <aayush.singh2100@gmail.com>
2025-07-02 23:51:26 +0530

    Added auth guard route

---
+++ bloggingWebsite_Client_Side/src/common/route-guards/auth-guard.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../../App';
+import Loader from '../../components/loader.component';
+import { Navigate } from "react-router-dom";
+
+const AuthGuardRoute = ({children}) => {
+  
+  const { UserAuth: {access_token}} = useContext(UserContext);
+
+  if(access_token === undefined) return <Loader />;
+  if(access_token === null) return <Navigate to="/signin" />
+  
+  return children;
+}
+
+export default AuthGuardRoute;
