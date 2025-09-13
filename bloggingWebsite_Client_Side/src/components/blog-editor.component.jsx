221825c85ee9761d5afa82b7e74145b20c5739f6
Ayush Singh <aayush.singh2100@gmail.com>
2025-09-13 23:40:41 +0530

    Added context for blog and handeled error for banner image

--- bloggingWebsite_Client_Side/src/components/blog-editor.component.jsx
+++ bloggingWebsite_Client_Side/src/components/blog-editor.component.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation.jsx";
-import blogBanner from "../imgs/blog banner.png"
+import defaultBanner from "../imgs/blog banner.png"
+import { EditorContext } from "../pages/editor.pages";
 
 const BlogEditor = () => {
 
+  const { blog , blog: { title, banner, content, tags, des }, setBlog} = useContext(EditorContext);
   const handleBannerUpload = (e) => {
     let file = e.target.files[0];
 
@@ -18,13 +20,25 @@ const BlogEditor = () => {
     }
   }
 
+  const handleTitleChange = (e) => {
+    console.log(e)
+    setBlog({...blog, title: e.target.value});
+  }
+
+  const handleBannerError = (e) => {
+    const bannerElement = e.target;
+    bannerElement.src = defaultBanner;
+  }
+
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="flex-none w-10">
           <img src={logo} />
         </Link>
-        <p className="text-black max-sm:hidden">New Blog</p>
+        <p className="text-black max-sm:hidden">
+          {title.length > 0 ? title : "New Blog"}
+        </p>
         <div className="flex gap-3 ml-auto">
           <button
             className="py-2 px-4 rounded-[2rem] bg-black text-grey 
@@ -39,14 +53,14 @@ const BlogEditor = () => {
             Save Draft
           </button>
         </div>
-      </nav>
+      </nav> 
 
       <AnimationWrapper>
         <section>
           <div className="mx-auto max-w-[720px] w-full grid justify-center">
-            <div className="relative bg-white aspect-video border-4 border-grey">
+            <div className="relative bg-white aspect-video border-4 border-grey lg:min-w-[40vw] md:min-w-[60vw] min-w-[80vw]">
               <label htmlFor="uploadBanner">
-                <img src={blogBanner}/>
+                <img src={banner} onError={handleBannerError}/>
                 <input id="uploadBanner" type="file" accept=".png, .jpg, .jpeg" hidden
                 onChange={handleBannerUpload}
                 >
@@ -59,6 +73,7 @@ const BlogEditor = () => {
             className="text-2xl font-[500] mt-3 w-full outline-none resize-none leading-tight text-start p-1"
             style={{fontFamily: 'cursive'}}
             onKeyDown={handleTitleKeydown}
+            onChange={handleTitleChange}
             >
 
             </textarea>
--- bloggingWebsite_Client_Side/src/pages/editor.pages.jsx
+++ bloggingWebsite_Client_Side/src/pages/editor.pages.jsx
@@ -1,18 +1,37 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, createContext } from 'react';
 import BlogEditor from '../components/blog-editor.component';
 import PublishForm from '../components/publish-form.component';
 
+const blogSchema = {
+  title: "",
+  banner: "",
+  content: [],
+  tags: [],
+  des: "",
+  author: {
+    personal_info: {}
+  }
+}
+
+export const EditorContext = createContext();
+
 const Editor = () => {
+
+  
+  const [blog, setBlog] = useState(blogSchema);
   const [EditorState, setEditorState] = useState("editor");
   return (
     <>
+    <EditorContext.Provider value={{blog, setBlog, EditorState, setEditorState}}>
       {
         EditorState == "editor" ? <BlogEditor /> :
         <PublishForm />
       }
+    </EditorContext.Provider>
+      
     </>
     
   )
 }
 
-export default Editor
+export default Editor;
